3e390a85c3b7b641c2ab761e4e434ed52c0a7305
lcmongwe <lcmongwe@gmail.com>
2022-03-04 19:14:26 +0300

    an array of akhan names added to js file

---
+++ js/script.js
@@ -0,0 +1,14 @@
+function akhanNames(){
+    var gender = document.getElementById("gender").value;
+    var birthday = document.getElementById("birthday").value;
+
+    akhanArray = [
+        ["Sunday" ,"Kwasi" ,"Akosua"]
+        ["Monday" ,"Kwado" ,"Adwoa"]
+        ["Tuesday" ,"Kwabena" ,"Abenaa"]
+        ["Wednesday" ,"Kwaku" ,"Akua"]
+        ["Thursday" ,"Yaw" ,"Yaa"]
+        ["Friday" ,"Kofi" ,"Afua"]
+        ["Saturday" ,"Kwame" ,"Ama"]
+    ]
+}
